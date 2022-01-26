70d541920f161a60b9a8b2d1c26c4f1aad8ff39d
Eduard <ptushko@gmail.com>
2022-01-26 16:47:47 +0300

    files transfer

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ app.innerHTML = `
     <h1>JavaScript HTML5 APIs</h1>
     <div class='uploader'>
         <h2>Upload your files</h2>
+        <p>Accept only .png, .jpg, .svg</p>
+        <input type='file' class='files' accept='image/*' multiple />
         <div class='dropzone'>🗂 Drag to Upload!</div>
+        <div class='list'></div>
     </div>
 
     <style>
@@ -42,6 +45,57 @@ app.innerHTML = `
 
 const init = () => {
     const dropzone = document.querySelector('.dropzone') as HTMLDivElement
+    const files = document.querySelector('.files') as HTMLInputElement
+    const list = document.querySelector('.list') as HTMLDivElement
+
+    const showFilePreview = (file: File) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.addEventListener('load', (e) => {
+            const div = document.createElement('div')
+            div.innerHTML = `
+                <div style='display: flex;'>
+                    <img
+                        src='${e.target?.result}'
+                        alt='${file.name}'
+                        style='width: 20px; margin-right: 10px;'
+                        />
+                    <p>${file.name} <span>${file.size} bytes</span></p>
+                </div>
+            `
+            list.append(div)
+        })
+    }
+
+    const uploadFiles = async (files: File[]) => {
+        const form = new FormData()
+        ;[...files].forEach((file) => form.append(file.name, file))
+
+        const request = await fetch('//dragdropfiles.glitch.me/upload', {
+            method: 'POST',
+            body: form,
+        })
+
+        return await request.json()
+    }
+
+    const isAllowedType = (file: File) => {
+        return ['image/png', 'image/jpeg', 'image/svg+xml'].includes(file.type)
+    }
+
+    const handleFileUpload = async (files: FileList) => {
+        const filesToUpload: File[] = [...files].filter(isAllowedType)
+
+        filesToUpload.forEach(showFilePreview)
+
+        const uploaded = await uploadFiles(filesToUpload)
+
+        if (uploaded) {
+            for (const image of uploaded.images) {
+                console.log(image)
+            }
+        }
+    }
 
     dropzone.addEventListener('dragenter', (e) => {
         ;(e.target as Element).classList.add('active')
@@ -64,7 +118,18 @@ const init = () => {
         e.stopPropagation()
         ;(e.target as Element).classList.remove('active')
 
-        const { files } = e.dataTransfer
+        const data = e.dataTransfer
+        if (data) {
+            handleFileUpload(data.files)
+        }
+    })
+
+    files.addEventListener('change', (e) => {
+        const data = e.target as HTMLInputElement
+
+        if (data) {
+            handleFileUpload(data.files!)
+        }
     })
 
     document.addEventListener('dragover', (e) => e.preventDefault())
